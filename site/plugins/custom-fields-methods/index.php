8eb5f86b5560fc69efbd44ae3a311c4656c760ef
azertypow <nico@villa1203.ch>
2025-07-02 17:11:35 +0200

    feat: add template custom methode fields

---
+++ site/plugins/custom-fields-methods/index.php
@@ -0,0 +1,29 @@
+<?php
+use Kirby\Cms\App;
+
+/**
+ * @var \Kirby\Cms\Page $page
+ * @var \Kirby\Cms\Site $site
+ * @var \Kirby\Cms\Pages $pages
+ */
+
+Kirby::plugin('villa1203/custom-fields-methods', [
+  'fieldMethods' => [
+    'toBlocks_custom' => function ($field) {
+
+      return $field->toBlocks()->map(function ($item) {
+        return [
+          'content' => $item->toArray(),
+          'img_srcset' => [
+            'tiny'  => $item->content()->image()->toFile()?->resize(50, null, 10)->url(),
+            'small' => $item->content()->image()->toFile()?->resize(500)->url(),
+            'reg'   => $item->content()->image()->toFile()?->resize(1280)->url(),
+            'large' => $item->content()->image()->toFile()?->resize(1920)->url(),
+            'xxl'   => $item->content()->image()->toFile()?->resize(2500)->url(),
+          ],
+        ];
+      })->data();
+
+    }
+  ]
+]);

e1b4766e4056ad7cbdaa6c09c29874815051dbb4
gabrielabergel <44164440+gabrielabergel@users.noreply.github.com>
2025-10-22 18:06:52 +0200

    update site yml

--- site/config/hooks.php
+++ site/config/hooks.php
@@ -12,7 +12,7 @@ return [
             // Compter combien d'articles ont déjà show_in_hero activé
             $heroArticlesCount = page('actualites')
                 // ->children()
-                // ->listed()
+                // ->listed()n
                 ->childrenAndDrafts()
                 ->filterBy('show_in_hero', 'true')
                 ->not($page) // Exclure l'article actuel
@@ -24,21 +24,6 @@ return [
             }
         }
 
-        // Vérifier si show_in_carousel est activé
-        if (isset($values['show_in_carousel']) && $values['show_in_carousel'] === true) {
-            // Compter combien d'articles ont déjà show_in_carousel activé
-            $carouselArticlesCount = page('actualites')
-                ->children()
-                ->listed()
-                ->filterBy('show_in_carousel', 'true')
-                ->not($page) // Exclure l'article actuel
-                ->count();
-
-            // Si on a déjà 5 articles, on empêche l'activation
-            if ($carouselArticlesCount >= 5) {
-                throw new Exception('Maximum 5 articles peuvent être affichés dans le carrousel de la page d\'accueil. Veuillez désactiver un autre article avant d\'activer celui-ci.');
-            }
-        }
     },
 
     'page.create:before' => function ($page, $input) {
@@ -56,18 +41,6 @@ return [
 
             if ($heroArticlesCount >= 2) {
                 throw new Exception('Maximum 2 articles peuvent être affichés dans le hero de la page d\'accueil.');
-            }
-        }
-
-        if (isset($input['show_in_carousel']) && $input['show_in_carousel'] === true) {
-            $carouselArticlesCount = page('actualites')
-                ->children()
-                ->listed()
-                ->filterBy('show_in_carousel', 'true')
-                ->count();
-
-            if ($carouselArticlesCount >= 5) {
-                throw new Exception('Maximum 5 articles peuvent être affichés dans le carrousel de la page d\'accueil.');
             }
         }
     }
